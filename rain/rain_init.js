3a9a43951c85dedf31294d9d4e4ef6700dee0ef3
boyu2011 <boyu2011@gmail.com>
2014-01-22 21:44:07 -0500

    changed image

--- rain/rain_init.js
+++ rain/rain_init.js
@@ -9,8 +9,12 @@ function run() {
         opacity: 1,
         fps: 30
     });
+    /*
     engine.trail = engine.TRAIL_DROPS;
     engine.rain([ [3, 2, 2] ], 100);
+    */
+    engine.rain([ [1, 2, 8000] ]);
+    engine.rain([ [3, 3, 0.88], [5, 5, 0.9], [6, 2, 1] ], 100);
 }
 
 $(window).load(function(){
