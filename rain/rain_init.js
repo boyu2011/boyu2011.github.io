eb28f1886b4d42040029ede0c18b3be222620a4c
boyu2011 <boyu2011@gmail.com>
2014-01-22 21:48:30 -0500

    change settings

--- rain/rain_init.js
+++ rain/rain_init.js
@@ -9,12 +9,7 @@ function run() {
         opacity: 1,
         fps: 30
     });
-    /*
-    engine.trail = engine.TRAIL_DROPS;
-    engine.rain([ [3, 2, 2] ], 100);
-    */
-    engine.rain([ [1, 2, 8000] ]);
-    engine.rain([ [3, 3, 0.88], [5, 5, 0.9], [6, 2, 1] ], 100);
+    engine.rain([ [0, 2, 200], [3, 3, 1] ], 100);
 }
 
 $(window).load(function(){
